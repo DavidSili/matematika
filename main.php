f46685b3bc27c223c0501610cbea73799f261950
david <agapetos@gmail.com>
2021-12-27 18:58:33 +0100

    css href

--- main.php
+++ main.php
@@ -10,7 +10,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name=viewport content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'>
-	<link rel="stylesheet" href="../assets/js/style.css?t=1">
+	<link rel="stylesheet" href="../assets/css/style.css?t=1">
 </head>
 <body>
 <div class="wrapper">
